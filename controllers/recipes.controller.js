2fcdb22a570239190c7998218bc095b4ac9a0313
Konkuri1 <konkurievarest363@gmail.com>
2024-03-26 10:04:17 +0000

    Error handling

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,21 +1,45 @@
 import { RecipeModel } from "../models/recipe.js";
 
-export const addRecipe = async (req, res) =>{
-    // Add recipe to the database
-    const createResult = await RecipeModel.create(req.body);
-    //Return result
-    res.status(201).json(createResult);
+export const addRecipe = async (req, res, next) =>{
+    try {
+        // Add recipe to the database
+        const createResult = await RecipeModel.create(req.body);
+        //Return result
+        res.status(201).json(createResult);
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const getRecipes = async (req, res) =>{
-    // Get all recipes from database
-    const findResult = await RecipeModel.find();
-    // Return response
-    res.status(200).json(findResult);
+export const getRecipes = async (req, res, next) =>{
+   try {
+     // Get all recipes from database
+     const findResult = await RecipeModel.find();
+     // Return response
+     res.status(200).json(findResult);
+   } catch (error) {
+        next(error);
+   }
 };  
 
-export const getRecipe = (req, res) =>{
-    res.send(`Get all recipe with id: ${req.params.id}`);
+export const getRecipe = async (req, res, next) =>{
+   try {
+    //Get single recipe by id
+    const findByIdResult = await RecipeModel.findById(req.params.id);
+    //Return 404 if no recipe is found
+    if (findByIdResult === null) {
+        res.status(404).json({
+            message:`Recipe with ID: ${req.param.id} not found!`
+        });
+    } else{
+    //Return response
+    res.status(200).json(findByIdResult);
+   } 
+} catch (error) {
+        next(error);
+    
+   }
+   
 };
 
 export const updateRecipe = (req, res) =>{
